c094b33c81b0d8ac62719208f247e806ebbad9ab
Lee Presswood <pressedlee123@gmail.com>
2016-03-23 21:13:01 -0500

    Fixed working directory issue.

--- app/core/src/com/leepresswood/NGame.java
+++ app/core/src/com/leepresswood/NGame.java
@@ -2,6 +2,7 @@ package com.leepresswood;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -9,7 +10,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class NGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
