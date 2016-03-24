858b0e702f7ff731d2283f546514d195191e782b
Lee Presswood <pressedlee123@gmail.com>
2016-03-23 21:06:43 -0500

    Added screen properties.

---
+++ app/core/src/com/leepresswood/constants/ApplicationConstants.groovy
@@ -0,0 +1,10 @@
+package com.leepresswood.constants
+/**
+ * Created by Lee on 3/23/2016.
+ */
+class ApplicationConstants {
+    public static final String TITLE = "N"
+    public static final int V_WIDTH = 320
+    public static final int V_HEIGHT = 240
+    public static final int SCALE = 2
+}
--- app/desktop/src/com/leepresswood/desktop/DesktopLauncher.java
+++ app/desktop/src/com/leepresswood/desktop/DesktopLauncher.java
@@ -3,10 +3,17 @@ package com.leepresswood.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.leepresswood.NGame;
+import com.leepresswood.constants.ApplicationConstants;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.title = ApplicationConstants.TITLE;
+		config.width = ApplicationConstants.SCALE * ApplicationConstants.V_WIDTH;
+		config.height = ApplicationConstants.SCALE * ApplicationConstants.V_HEIGHT;
+		config.resizable = false;
+
 		new LwjglApplication(new NGame(), config);
 	}
 }
